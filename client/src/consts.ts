79da1280fb72de19077e3679257cec6df2257ec2
Kariem816 <kariem816@gmail.com>
2026-03-16 01:04:00 +0200

    fix fixed enum length

--- client/src/consts.ts
+++ client/src/consts.ts
@@ -27,7 +27,7 @@ export enum TWeapon {
 	WEAPON_UZI,
 	WEAPON_BOMB,
 };
-export const WeaponsCount = 5; // remember to update this every time you change weapons
+export const WeaponsCount = Object.keys(TWeapon).length / 2; // ts enum double reference bs
 
 export const theme = {
 	colors: {
